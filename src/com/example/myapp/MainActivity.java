c581509bd92ae9c1936d28d261908d7df7c8a80f
wanghui <huiwang@cienet.com.cn>
2015-05-18 10:42:32 +0800

    add handler cases

--- src/com/example/myapp/MainActivity.java
+++ src/com/example/myapp/MainActivity.java
@@ -8,6 +8,7 @@ import android.content.Intent;
 import android.content.ServiceConnection;
 import android.os.Bundle;
 import android.os.Handler;
+import android.os.HandlerThread;
 import android.os.IBinder;
 import android.os.Message;
 import android.util.Log;
@@ -28,7 +29,11 @@ public class MainActivity extends Activity implements View.OnClickListener {
     boolean mBound = false;
     Button playBtn;
     Button handlerBtn;
-    
+    Button threadHandlerBtn;
+
+    private static final int SEND_MESSAGE_TO_UITHREAD = 0;
+    private static final int WAIT = 0;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -53,20 +58,24 @@ public class MainActivity extends Activity implements View.OnClickListener {
         loaderBtn.setOnClickListener(this);
         handlerBtn = (Button) findViewById(R.id.handler);
         handlerBtn.setOnClickListener(this);
+        threadHandlerBtn = (Button) findViewById(R.id.threadHandler);
+        threadHandlerBtn.setOnClickListener(this);
+        Button handlerthreadBtn = (Button) findViewById(R.id.handlerthread);
+        handlerthreadBtn.setOnClickListener(this);
         intent = new Intent();
     }
 
     @Override
     public void onClick(View v) {
         // TODO Auto-generated method stub
-        switch(v.getId()){
+        switch (v.getId()) {
             case R.id.cal:
                 intent.setClass(MainActivity.this, Calculate.class);
                 startActivity(intent);
                 break;
-                //localbinder service
+            // localbinder service
             case R.id.play:
-                if (playBtn.getText().equals("play music")){
+                if (playBtn.getText().equals("play music")) {
                     playBtn.setText("stop music");
                     onStart();
                     mService.play();
@@ -96,12 +105,12 @@ public class MainActivity extends Activity implements View.OnClickListener {
                 intent.setClass(MainActivity.this, MutilTouchDemoActivity.class);
                 startActivity(intent);
                 break;
-                //loader sample
+            // loader sample
             case R.id.applist:
                 intent.setClass(MainActivity.this, ContactSampleListActivity.class);
                 startActivity(intent);
                 break;
-                //provider loader
+            // provider loader
             case R.id.loader:
                 intent.setClass(MainActivity.this, LoaderThrottle.class);
                 startActivity(intent);
@@ -113,16 +122,46 @@ public class MainActivity extends Activity implements View.OnClickListener {
                 Log.i("myapp", "AfterSendMessage");
                 try {
                     synchronized (Thread.currentThread()) {
-                        Thread.currentThread().wait(5000);
-                      }
+                        Thread.currentThread().wait(3000);
+                    }
                 } catch (InterruptedException e) {
                     // TODO Auto-generated catch block
                     e.printStackTrace();
                 }
+                break;
+            case R.id.threadHandler:
+                myThread.start();
+                break;
+            case R.id.handlerthread:
+                HandlerThread ht = new HandlerThread("test");
+                ht.start();
+                // handler是在它关联的looper线程中处理消息的
+                Handler handler = new Handler(ht.getLooper()) {
+
+                    @Override
+                    public void handleMessage(Message msg) {
+                        // TODO Auto-generated method stub
+                        super.handleMessage(msg);
+                        switch (msg.what) {
+                            case WAIT:
+                                Toast.makeText(
+                                        MainActivity.this,
+                                        "The current thread name:"
+                                                + Thread.currentThread().getName(),
+                                        Toast.LENGTH_SHORT).show();
+                                System.out
+                                        .println("Handler--->" + Thread.currentThread().getName());
+                                break;
+                        }
+
+                    }
+
+                };
+                handler.obtainMessage(WAIT).sendToTarget();
             default:
                 break;
         }
-        
+
     }
 
     @Override
@@ -149,7 +188,8 @@ public class MainActivity extends Activity implements View.OnClickListener {
         @Override
         public void onServiceConnected(ComponentName className,
                 IBinder service) {
-            // We've bound to LocalService, cast the IBinder and get LocalService instance
+            // We've bound to LocalService, cast the IBinder and get
+            // LocalService instance
             LocalBinder binder = (LocalBinder) service;
             mService = binder.getService();
             mBound = true;
@@ -160,9 +200,9 @@ public class MainActivity extends Activity implements View.OnClickListener {
             mBound = false;
         }
     };
-    
+
     private class TestHandler extends Handler {
-        
+
         private static final int COUNT = 0;
         private static final int STOP_COUNT = 1;
         int count = 0;
@@ -173,11 +213,12 @@ public class MainActivity extends Activity implements View.OnClickListener {
             super.handleMessage(msg);
             switch (msg.what) {
                 case COUNT:
-                    if (count < 10){
+                    if (count < 10) {
                         Log.i("myapp", "in the message");
                         count++;
-                        Toast.makeText(MainActivity.this, "count："+ count, Toast.LENGTH_SHORT).show();
-                        sendEmptyMessageDelayed(TestHandler.COUNT,4000);
+                        Toast.makeText(MainActivity.this, "count：" + count, Toast.LENGTH_SHORT)
+                                .show();
+                        sendEmptyMessageDelayed(TestHandler.COUNT, 4000);
                     } else {
                         sendEmptyMessage(TestHandler.STOP_COUNT);
 
@@ -185,9 +226,45 @@ public class MainActivity extends Activity implements View.OnClickListener {
                     break;
                 default:
                     break;
-                    
+
             }
         }
-        
+
     }
+
+    private Handler uiHandler = new Handler() {
+
+        @Override
+        public void handleMessage(Message msg) {
+            // TODO Auto-generated method stub
+            super.handleMessage(msg);
+            switch (msg.what) {
+                case SEND_MESSAGE_TO_UITHREAD:
+                    threadHandlerBtn.setTextColor(0);
+            }
+        }
+
+    };
+
+    private Thread myThread = new Thread() {
+
+        @Override
+        public void run() {
+            // TODO Auto-generated method stub
+            super.run();
+            for (int i = 0; i < 3; i++) {
+                try {
+                    synchronized (Thread.currentThread()) {
+                        Thread.currentThread().wait(2000);
+                    }
+                } catch (InterruptedException e) {
+                    // TODO Auto-generated catch block
+                    e.printStackTrace();
+                }
+            }
+            uiHandler.obtainMessage(SEND_MESSAGE_TO_UITHREAD).sendToTarget();
+        }
+
+    };
+
 }
